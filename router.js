5b7c6e75e0d8e701f53487a9eb14d085aeb6d0e6
javiergp23 <javgon23@hotmail.com>
2022-12-23 17:15:54 -0300

    first commit

---
+++ router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const conexion = require('./database/db')
+
+router.get('/', (req, res)=>{
+    conexion.query('SELECT * FROM users', (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    }) 
+})
+
+module.exports = router;
